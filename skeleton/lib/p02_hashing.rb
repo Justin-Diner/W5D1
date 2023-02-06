bfb64b8ad44d2f1d33cbd5a35758a3706b0d2d07
Justin <jcdiner@gmail.com>
2023-02-06 16:59:04 -0500

    complete string and array hash

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,25 @@ end
 
 class Array
   def hash
-  end
+	res = []
+
+	self.each_with_index do |ele, idx|
+		res << ((ele.hash).to_s + (self.length % (idx + 1)).to_s).to_i
+	end
+	res.sum
+	end
 end
 
 class String
   def hash
+	res = ""
+	alphabet = ("a".."z").to_a
+
+	self.each_char.with_index do |letter, idx|
+		#res << ((letter.to_i.hash).to_s + (self.length % (idx + 1)).to_s).to_i
+		res = res + (alphabet.index(letter).hash).to_s + (self.length % (idx + 1)).to_s
+	end
+	res.to_i
   end
 end
 
@@ -16,6 +30,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    
   end
 end
