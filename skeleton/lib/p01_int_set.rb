ac1491c4f6d5e2fc11760c84a0b010d9e232f53a
Justin <jcdiner@gmail.com>
2023-02-06 14:14:47 -0500

    IntSet complete

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,18 +1,21 @@
 class MaxIntSet
+	attr_accessor :store
+
   def initialize(max)
     @max = max
-    @store = Array.new(@max,false)
+    @store = Array.new(@max, false)
   end
 
   def insert(num)
     if num > @max || num < 0
-      raise "Out of range"
+      raise "Out of bounds"
     else
       @store[num] = true
     end
   end
 
   def remove(num)
+	@store[num] = false
   end
 
   def include?(num)
@@ -30,17 +33,27 @@ end
 
 
 class IntSet
+	attr_accessor :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+	@num_buckets = num_buckets
   end
 
   def insert(num)
+	@store[num % 20] << num
   end
 
   def remove(num)
+	@store.each do |subArr|
+		if subArr.include?(num)
+			subArr.delete(num)
+		end
+	end
   end
 
-  def include?(num)
+  def include?(num) 
+	@store.any? { |subArr| subArr.include?(num)}
+
   end
 
   private
